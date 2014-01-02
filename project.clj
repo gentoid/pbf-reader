579187b5630e681bd9af3357dfc5daf6f1475b92
Viktor Lazarev <taurus101v@gmail.com>
2014-01-02 23:24:35 +0400

    Add protobuf deps

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.flatland/protobuf "0.8.1"]]
+  :plugins [[lein-protobuf "0.1.1"]])
