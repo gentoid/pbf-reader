7a61a05a4335e5c11cbcf1f2a71aa58d7987e2ca
Viktor Lazarev <taurus101v@gmail.com>
2014-01-02 22:13:57 +0400

    Create new project by lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pbf-reader
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/pbf_reader/core_test.clj
@@ -0,0 +1,7 @@
+(ns pbf-reader.core-test
+  (:require [clojure.test :refer :all]
+            [pbf-reader.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
