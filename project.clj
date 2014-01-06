a41842182a0c2e8a3a131735f3dea72d392dc19a
Viktor Lazarev <taurus101v@gmail.com>
2014-01-06 13:31:34 +0400

    dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.flatland/protobuf "0.8.1"]
                  [clj-configurator "0.1.5"]
-                 [scrosby/osmpbf "1.3.1"]]
+                 [gentoid/osmpbf "1.3.1"]]
   :plugins [[lein-protobuf "0.1.1"]]
   :main pbf-reader.core
   :profiles {:dev {:dependencies [[spyscope "0.1.4"]]}}
