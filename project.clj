c9e1afea9fee8a1991c2455e3fd54d97bdf50822
Viktor Lazarev <taurus101v@gmail.com>
2014-02-06 01:22:42 +0400

    Project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pbf-reader "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "OSM PBF parser & reader"
+  :url "https://github.com/gentoid/pbf-reader"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
